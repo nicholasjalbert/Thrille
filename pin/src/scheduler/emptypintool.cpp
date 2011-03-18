9639313b5c5ccabc29b1015b247f1160bce25162
Nick Jalbert <jalbert@cs.berkeley.edu>
2011-03-18 16:04:00 -0700

    empty pin tool

---
+++ pin/src/scheduler/emptypintool.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author - Nick Jalbert (jalbert@cs.berkeley.edu)
+ *
+ */
+
+#include "pin.H"
+
+int main(int argc, char* argv[]) {
+    PIN_Init(argc, argv)
+    PIN_StartProgram();
+    return 0;
+}
